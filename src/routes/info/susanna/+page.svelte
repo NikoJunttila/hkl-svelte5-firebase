14e7236739a12435f2128123ce53174884c1cf7d
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-07-26 09:11:53 +0300

    info page

--- src/routes/info/susanna/+page.svelte
+++ src/routes/info/susanna/+page.svelte
@@ -1,19 +1,56 @@
 
 
 <div class="max-w-[50ch]">
-    <h2 class="pb-5 text-2xl font-bold">Heissan Sinulle!</h2>
-    <p>Olen Susanna Höijer ja toimin yrittäjänä Hyvinvointikeskus Luxuksessa
-Olen toiminut yrittäjänä; Parantajana, näkijänä ja hierojana vuodesta 2002 saakka sekä
-kouluttanut myös muita toimimaan energiaparantajina.</p>
-<p>Selvänäkö ja energiahoitaminen on kuitenkin kulkenut mukanani jo ihan lapsuudestani saakka
-sillä olen aina ollut herkkä aistimaan ja näkemään energioita ja asioita selvänäkemisen kautta.</p>
-<p>Reilut parikymmentä vuotta olen ollut kiinnostunut henkisen hyvinvoinnin
-merkityksestä fyysisen kehon huoltamisessa ja olen omaa itseäni kehittänyt niin hyvinvoinnin
-kuin tietoisuuteni kautta ja tämän pohjalta kuin myös saamieni koulutuksien pohjalta olen luonut
-oman konseptin Hyvinvointikeskus Luxusen hyvinvointipalveluihin. Siispä katson Sinun tilaasi
-aina kokonaisvaltaisesti on kyseessä sitten hieronta tai muu vaihtoehtoinen hyvinvointipalvelu
-jonka valitset tueksi ja oppaaksi matkallesi.</p>
-    <p>Hoidot eivät ole lääketieteellisiä eivätkä korvaa mitään lääketieteellistä hoitoa.</p>
+    <h2 class="pb-5 text-2xl font-bold">Heissan!</h2>
+    Heissan! 
+    <br>
+    Kaikkein tärkein asia jotta hyvinvointi, terveys ja tasapaino elämässä voi kukoistaa on se, että tulisi ensin oppia rakastamaan itseään.
+    Silloin osaa hyväksyä itsensä täysin, tehdä itseä kunnioittavia ja arvostavia valintoja ja asettaa terveet rajat itselle ja toisille.
+    
+    <br>
+    Olen Susanna Höijer Hyvinvointikeskus Luxusen yrittäjä. Olen toiminut parantajana, näkijänä ja henkisenä valmentajana ja koulutettuna hierojana yli 20 vuoden ajan.
+    
+    <br>
+    Erityisesti alaani on löytää ja nähdä  henkinen ydinsyy ongelmaan joka asiakkaalla on. Ongelma voi liittyä sairauteen tai terveys haasteisiin, fyysiseen kehoon, parisuhteen epätasapainoon, itsetunto ongelmiin, ja traumoihin, ja sellaisiin asioihin jotka jolloin tavalla estää eteenpäin menemisen tai aiheuttaa sisäistä ahdistusta.
+    
+    <br>
+    Helppoa olisi saada valmiit vastaukset kaikkeen mutta se ei tuo sitä muutosta jonka ydinongelma tarvitsee. Siksi haluan palveluillani ohjata asiakasta itseä löytämään ja näkemään omat rajoittavat asiat ja sen, että mitä hänen tulisi elämässään muuttaa jotta voisi voida hyvin.
+    
+    <br>
+    <span class="font-bold text-xl">MIKSI NÄKIJÄ JA PARANTAJA?</span><br>
+    Olen aina ollut näkijä jo lapsesta saakka. Myös keskustellut enkeleiden kanssa. Parantaminen ja kaikki parantamiseen liittyvät hoitomuodot kuten ovat astuneet mukaan kun 2001 vuonna opiskelin hierojakoulussa. Tämä koulutus avasi käsieni kautta kulkevat parannuskanavat. Kun teen töitä olen aina yhteydessä korkeimpaan valovoimaan Luojaan. Ja  kaikki hoitojen avulla tehtävät muutokset tapahtuvat taajuus tasojen kautta. 
+    
+    <br>
+    <span class="font-bold text-xl">ISOISÄNI</span> oli selvännäkijä. Joten on tuntunut niin luonnolliselta asialta itselleni se, että minäkin olen saanut olla näkijä ja syntyä näkijäksi. On ihana kiitollisuuden tunne tästä kaikesta. 
+    
+    <br>
+    Olen saanut kulkea omia muutos matkoja joita käsittelemällä olen  eheyttänyt itseäni. Nämä kaikki matkat ovat olleet suuria opettajia joista olen saanut valtavasti lisää tietoisuutta, näkemystä ja hoitavaa energiaa mukaan Hyvinvointikeskus Luxusen palveluihin. 
+    
+    <br>
+    Olen luonut oman hoito ja valmennus konseptin. Katson asiakkaan tilaa aina kokonaisvaltaisesti ja hoito valikoituu sen mukaisesti mikä parhaiten tukee asiakkaan omaa muutosmatkaa. 
+    
+    <br>
+    Parantajan tulee ensin osata hoitaa itseään ennenkuin voi aloittaa muiden hoitamisen ja tämä sääntö on alusta saakka ollut mukana yritykseni toiminnassa. Näitä samoja työvälineitä joita käytän asiakkaille hoidoissani käytän edelleen myös itseni uudistamiseen. Työvälineissä, jotka sisältävät vahvaa muutosvoimaa on mukana suuri viisaus, valo ja rakkaus.
+    
+    <br>
+    <p class="font-bold text-xl">KAIKELLA ON AINA JOKIN TARKOITUS</p>
+    Tarkoituksen saa tietää silloin kun on valmis kuulemaan vastauksen.
+    
+    <br>
+    <span class="font-bold text-xl">JOS SINULLA</span> on jokin asia johon haluat saada selkeyttä, parannus voimaa ja muutosta - olethan epäröimättä yhteydessä. <a href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIAbACxMAcAzEwAwBMPA7DwCG-GkIYMAjDSb92EdiAA0IITRpxMcDEIA2AZWjwkYWgRIBdAL7WgA">Varaa esimerkiksi ilmainen kartoitus aika, käydään asiaa läpi ja katsotaan voinko olla Sinulle avuksi. </a>
+    
+    <br>
+    <span class="font-bold text-xl">MIKÄLI</span> koet, että haluat kehittää omia parantajan taitojasi, ole yhteydessä silloinkin. Olen pitänyt Parantajakoulutuksia vuodesta 2005 saakka. Ehkä tämä koulutus voisi resonoida sinullekin. 
+    
+    <br>
+    Kiitos Kun Olet.
+    <br>
+    Kaikkea hyvää ja parasta päivään meille kaikille! 
+    
+    <br>
+    Valon säteillä,
+    <br>
+    Susanna Höijer 
     <br>
     <h4 class="font-bold">Koulutukset</h4>
     <p>Kodinhoitajan ammattitutkinto, Kauhajoki 1991</p>
