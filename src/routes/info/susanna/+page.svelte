d150cb3cea13cc6bd931dcb2e727add46ae1595b
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-08-01 16:27:01 +0300

    brrrrrrrrrr

--- src/routes/info/susanna/+page.svelte
+++ src/routes/info/susanna/+page.svelte
@@ -2,12 +2,13 @@
 
 <div class="max-w-[50ch]">
     <h2 class="pb-5 text-2xl font-bold">Heissan!</h2>
-    Heissan! 
     <br>
+    
     Kaikkein tärkein asia jotta hyvinvointi, terveys ja tasapaino elämässä voi kukoistaa on se, että tulisi ensin oppia rakastamaan itseään.
     Silloin osaa hyväksyä itsensä täysin, tehdä itseä kunnioittavia ja arvostavia valintoja ja asettaa terveet rajat itselle ja toisille.
     
     <br>
+    
     Olen Susanna Höijer Hyvinvointikeskus Luxusen yrittäjä. Olen toiminut parantajana, näkijänä ja henkisenä valmentajana ja koulutettuna hierojana yli 20 vuoden ajan.
     
     <br>
@@ -17,9 +18,12 @@
     Helppoa olisi saada valmiit vastaukset kaikkeen mutta se ei tuo sitä muutosta jonka ydinongelma tarvitsee. Siksi haluan palveluillani ohjata asiakasta itseä löytämään ja näkemään omat rajoittavat asiat ja sen, että mitä hänen tulisi elämässään muuttaa jotta voisi voida hyvin.
     
     <br>
+    <br>
     <span class="font-bold text-xl">MIKSI NÄKIJÄ JA PARANTAJA?</span><br>
+    <br>
     Olen aina ollut näkijä jo lapsesta saakka. Myös keskustellut enkeleiden kanssa. Parantaminen ja kaikki parantamiseen liittyvät hoitomuodot kuten ovat astuneet mukaan kun 2001 vuonna opiskelin hierojakoulussa. Tämä koulutus avasi käsieni kautta kulkevat parannuskanavat. Kun teen töitä olen aina yhteydessä korkeimpaan valovoimaan Luojaan. Ja  kaikki hoitojen avulla tehtävät muutokset tapahtuvat taajuus tasojen kautta. 
     
+    <br>
     <br>
     <span class="font-bold text-xl">ISOISÄNI</span> oli selvännäkijä. Joten on tuntunut niin luonnolliselta asialta itselleni se, että minäkin olen saanut olla näkijä ja syntyä näkijäksi. On ihana kiitollisuuden tunne tästä kaikesta. 
     
@@ -32,14 +36,14 @@
     <br>
     Parantajan tulee ensin osata hoitaa itseään ennenkuin voi aloittaa muiden hoitamisen ja tämä sääntö on alusta saakka ollut mukana yritykseni toiminnassa. Näitä samoja työvälineitä joita käytän asiakkaille hoidoissani käytän edelleen myös itseni uudistamiseen. Työvälineissä, jotka sisältävät vahvaa muutosvoimaa on mukana suuri viisaus, valo ja rakkaus.
     
-    <br>
+    <br><br>
     <p class="font-bold text-xl">KAIKELLA ON AINA JOKIN TARKOITUS</p>
     Tarkoituksen saa tietää silloin kun on valmis kuulemaan vastauksen.
     
-    <br>
+    <br><br>
     <span class="font-bold text-xl">JOS SINULLA</span> on jokin asia johon haluat saada selkeyttä, parannus voimaa ja muutosta - olethan epäröimättä yhteydessä. <a href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIAbACxMAcAzEwAwBMPA7DwCG-GkIYMAjDSb92EdiAA0IITRpxMcDEIA2AZWjwkYWgRIBdAL7WgA">Varaa esimerkiksi ilmainen kartoitus aika, käydään asiaa läpi ja katsotaan voinko olla Sinulle avuksi. </a>
     
-    <br>
+    <br><br>
     <span class="font-bold text-xl">MIKÄLI</span> koet, että haluat kehittää omia parantajan taitojasi, ole yhteydessä silloinkin. Olen pitänyt Parantajakoulutuksia vuodesta 2005 saakka. Ehkä tämä koulutus voisi resonoida sinullekin. 
     
     <br>
@@ -51,6 +55,7 @@
     Valon säteillä,
     <br>
     Susanna Höijer 
+    <br><br>
     <br>
     <h4 class="font-bold">Koulutukset</h4>
     <p>Kodinhoitajan ammattitutkinto, Kauhajoki 1991</p>
