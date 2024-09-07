f55dae0aafc8882e6480fe38db0e3e8da23b367d
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-09-07 19:28:53 +0300

    cba

--- src/routes/matka/+page.svelte
+++ src/routes/matka/+page.svelte
@@ -1,93 +1,126 @@
 <main class="ml-2 py-10 flex flex-col justify-center align-middle ">
     <div class="grid place-items-center pb-5">
-        <div class="max-w-[100ch] space mx-1">
-<h1 class="text-2xl font-bold">Muutoksen matka 3kk premium valmennus</h1>
-      <br>
-            <p>Miltä kuulostaisi, jos voisit kolmen kuukauden kuluttua kokea ja tuntea uskomattomia muutoksia itsessäsi?</p>
-            
-            <p>*Saada oman sisäisen voiman ja osaat asettaa rajat ja sanoa "ei" ilman, että se tuntuu epämiellyttävältä.</p>
-            
-            <p>*Olet vapaa miellyttämisen, riittämättömyyden ja arvottomuuden kahleista.</p>
-            
-            <p>*Huomaat itsesi päivä päivältä eheytyneeksi.</p>
-            
-            <p>*Saat itsevarmuutta.</p>
-            
-            <p>*Keholta vapautuu tunteiden aiheuttamat taakat ja stressi.</p>
-            
-            <p>*Opit täysin itsesi hyväksymisen ilman, että kannat enää mukana katkeruutta, vihaa, inhoa, pettymystä.</p>
-            
-            <p>*Lopputuloksena on, että tiedät miltä tuntuu rakastaa, arvostaa ja kunnioittaa itseäsi.</p>
-            
-            <p>*Saat lisäksi myös fyysinen kehon tasapainoon.</p>
-            
-            <p>*Ja näet tulevaisuutesi valoisana ja täynnä toivoa.</p>
-            
-            <p>Kuulostaa hyvältä, eikö?</p>
-            <br>
-            <p>Tämä on sinulle mahdollista 3 kk Muutoksen matka premium valmennuksella.</p>
-            
-            <p>Valmennukseen saat mukaasi tuen koko 12 viikon ajalle.</p>
-            
-            <p>Autan yksinkertaisilla menetelmillä sinua sisäistämään omaa itseäsi ja vapautumaan kaikesta siitä tunnekuormasta, joka on itsesi rakastamisen ja eheytymisesi esteenä.</p>
-            
-            <p>Mitä tämä kaikki käytännössä tarkoittaa?</p>
-            
-            <p>Opetan 3 kk aikana sinulle asteittain työvälineet joilla eheytät ja voimaannutat sisimpäsi ja fyysisen kehosi. Opetan kuinka rakennat uuden konseptin itsellesi joka tukee myös sinun henkistä kasvua ja tiedät miten mennä elämässä eteenpäin näillä uusilla työvälineillä.</p>
-            
-            <p>1. kk vapautat vanhat menettely -, toiminta -, ajattelu -, ja tunne mallit. Löydät sisäisen valosi ja rakkauden itseäsi kohtaan, näet oman arvosi ja avaudut tietoisuudellesi. Tunnet itsesi henkisesti vahvaksi, olet vapaa riittämättömyydestä ja miellyttämisestä ja saat tasapainon ja tiedostat omat rajat joilla mennä helpottuneena eteenpäin.</p>
-            
-            <p>2. kk löydät yhteisymmärryksen kehosi kanssa. Vapautat keholta stressin ja löydät uusia keinoja hoitaa kehosi tasapainoon Olet sinut itsesi ja kehosi energiakenttien kanssa.</p>
-            
-            <p>3. kk osaat reitit mennä eteenpäin, näet tulevaisuutesi valoisana, positiivisena, ja onnellisena. Rakastat itseäsi ja elämääsi. Olet jättänyt taaksesi vanhan sinän ja uusi sinä tietää kuinka kukoistaa ja manifestoida haluamiaan asioita elämäänsä.</p>
-            
-            <p>Muutoksen matka valmennus toimii etänä tehtävillä työkirjoilla ja lisäksi 1-2 viikon välein otetaan livevalmennukset videotapaamisina. Sinulla on koko kolmen kuukauden matkan aikana Susannan tuki mukana.</p>
-            
-            <p>Kuulostaako tämä Muutoksen matka Sinun matkalta?</p>
-            <br>
-
-            <p>Tälle valmennus matkalle annan sinulle myös täyden takuun 12 viikon yhteistyömme ajaksi. Takaan, että tämä selkeä ja systemaattinen valmennus ohjelma auttaa sinut 12 viikossa epätasapainosta selkeyteen ja oman voimasi löytämiseen ja itsesi rakastamiseen.</p>
-            
-            <p>Kun haluat tietää tästä kaikesta lisää ja miettiä olisiko tämä valmennus Sinulle sopiva, niin varaa tästä
-        <a href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIA7AIYAcADBAgCysRcCsENgE4AZv2EQGANgBGARn4gANCCY0acTHAxMANgGVo8JGFoESAXQC%2BNoA" class="bigger">ILMAINEN alkukartoitus</a> info aika, niin jutellaan lisää ja katsotaan olisiko tämä matka sinulle sopiva.</p>
-      
-            
-            <p>Kartoitus ei sido sinua mihinkään, mutta voi antaa sinulle ison harppauksen eteenpäin omaan muutokseesi.</p>
-            
-            <p>Muutoksen matka premium valmennuksen mentorina valmentaja on Susanna. "Olen myös itse käynyt läpi tämän muutos matkan, joten tiedän mikä toimii ja mikä ei. Ja olen kehitellyt tähän valmennukseen Sinulle vain kaikista parhaimmat ja tarpeelliset työvälineet, jotta pääset eheytymään itsesi kanssa."</p>
-            <br>
-            <p>Tuloksia valmennettavilta:</p>
-            
-            <p>"Arvostus itseäni kohtaan on noussut ja sitä myötä arvostan myös muita ihmisiä aidosti. Olen myös huomannut, että tulevaisuuden suunnittelu ja ihan vain haaveilu on helpompaa, siihen ei liity sellaista negatiivista 'ei se kuitenkaan tapahdu' - ajattelumallia niinkuin aiemmin."</p>
-            
-            <p>"Tuntuu, että olen pystynyt jättämään asioita jotka ovat aiheuttaneet alitajuntaisesti tuskaa. On kevyempi olo."</p>
-            
-            <p>"On turvallinen ja hyvä olo tehtäviä tehdessä. Susannan tuki on valoisaa ja positiivista. Koen, että minut hyväksytään sellaisena kuin olen ja uskallan sanoa asioita ääneen koska tuntuu ettei tuomitse mistään sanomisista. Iso kiitos Susanna!"</p>
-            
-            <p>"Valmennus ei ole pitkästyttävää puurtamista. Jokainen tehtäväkirja avaa aina lisää omaa itseä ja pitää mielenkiinnon yllä. Tehtävissä on kuitenkin myös haastetta ja tekemistä ja niihin pitää tosissaan keskittyä. Ei riitä vaan että tekstit lukee läpi. Se ei vielä tee muutosta. Muutos tapahtuu vasta kun tekee annetut tehtävät."</p>
-            
-            <p>Varaa oma alkukartoitus nyt. Käydään yhdessä läpi sinun haasteet ja tavoitteet ja sitten vasta katsotaan sopiiko tämä valmennus sinulle ja olisitko valmis sitoutumaan siihen.</p>
-            
-            <p>Varaa
-        <a class="bigger" href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIA7AIYAcADBAgCysRcCsENgE4AZv2EQGANgBGARn4gANCCY0acTHAxMANgGVo8JGFoESAXQC%2BNoA">ilmainen ALKUKARTOITUS TÄSTÄ</a></p>
-            <br>
-        <p><a class="bigger" href="https://youtu.be/gpQRGQSqLz0?si=lPuUps_guo_DKw36">OTA TÄSTÄ</a> testaukseen harjoitus, jonka avulla voit opetella rakastamaan ja hyväksymään itsesi.</p>
-    <br>  
-    <img class="my-2 rounded-lg" src="https://i.imgur.com/fwZsbNC.jpeg" alt="muutoksen matka"/>
-    Valmennuksen hinta on 2000€ (sis.alv 25.5%) maksetaan kokonaisuudessaan ennen valmennuksen aloitusta. 
+        <div class="max-w-[100ch] space mx-1 text-lg">
+<h1 class="text-3xl font-bold">TUTUSTUMINEN OMAAN SISIMPÄÄSI? </h1>
+<img src="https://i.imgur.com/41nqpOT.png" alt="idk">
 <br><br>
-Mahdollisuus myös maksaa erissä. Erämaksuihin lisätään yht. 10% lisä<br>
-Kysy lisää erämaksu mahdollisuudesta kartoituspuhelussa. 
+Tämä uudistettu Muutoksen matka valmennus valmistaa sinulle tukevan perustan, joka vahvistaa sinut henkisesti ja fyysisesti. Sinulla on tämän valmennuksen käytyäsi aina oma työkalupakki mukana josta voit kulloiseenkin tarpeeseen tai matkan varrella olevaan haasteeseen ottaa oman työvälineen jolla sujuvasti pääset eteenpäin.
+<br><br>
 
-        </div>
+Muutoksen matkan tarkoitus on valmentaa sinut aidosti rakastamaan itseäsi. Ei riitä että ajattelee rakastavansa itseään jos teot eivät ole samassa linjassa ajatuksen kanssa. 
+<br><br>
+
+Yksi parhaimmista keinoista selvittää se että rakastaako itseään on mennä peilin eteen ja katsoa miltä oma peilikuva tuntuu. Tuntuuko se, että "jes, olen upea" vai tuntuuko se, että " voi ei, en edes välitä katsoa itseäni peilistä, tai onko ajatuksesi enemmän sitä luokkaa , että  "se on turhaa katsoa peilistä itseä". Tai kenties "ei sieltä kuitenkaan mitää hyvää näy,". 
+<br><br>
+
+Myös se kertoo paljon, että millä tavalla itsestään ajattelee. Onko tapana enemmän moittia tai sättiä itseä kuin olla kiitollinen itsestään ja rakastaa itseään. 
+Kun aidosti rakastaa itseä osaa tehdä itseä kunnioittavia ja arvostavia valintoja. Itseään kunnioittava ei koskaan moiti eikä mitätöi itseä. 
+<br><br>
+<img src="https://i.imgur.com/flGpNjz.png" alt="idk">
+👉 Jos mielit auttaa aina kaikessa muita ja etenkin laitat kaikki muut aina edelle oman elämäsi ja itsesi kustannuksella, ei tarkoita sitä, että se olisi rakkautta sinua itseäsi kohtaan. Päinvastoin se on tuho energiaa sinua itseäsi kohtaan. 
+<br><br>
+<p class="font-bold text-2xl">MUUTOKSEN MATKAN</p>
+<br><br>
+<img src="https://i.imgur.com/pztzqCg.png" alt="idk">
+1. kuukauden osa-alueella opit löytämään rajoittavat tunteet ja ohjelmoinnit itseltäsi, opit vapautuu suurimmista peloista ja merkityksettömyydestä ja niistä kaikista jotka ovat estäneet sinua olemasta omassa voimassasi, riittävä, ja tärkeä. Tämän ensimmäinen kuukauden toisella viikolla opit rakastamaan itseäsi ja tiedät, miltä se tunne kuuluu tuntua.
+<br><br>
+
+Silloin jää pois toisten miellyttämiset, itsesi pienentäminen ja näkymättömän lapsen roolin kantaminen. Nouset omaan voimaasi, kukoistukseen ja ihanaan itseäsi kohtaan 
+tuntemaan rakkauden voimaan. 
+
+<br><br>
+<img src="https://i.imgur.com/TqkcnGO.png" alt="idk">
+MUUTOKSEN MATKA valmentaa sinua rakastaa myös oma kehosi terveeksi. 
+Toisen kuukauden osa-alue koostuu fyysisen kehon aiheista ja siitä mitä kaikkia tunteita fyysinen keho pitää sisällään esimerkiksi kipujen ja oireilun kautta. 
+Opit fyysisen kehon ja tunne ohjelmoinnin yhteistyöstä tärkeät seikat. 
+Tämän osion käytyäsi tunnet kehosi terveyden ja tunnet jatkossa sinun sisäisen ja fyysisen kehosi yhteenkuuluvuuden. Opit lukemaan kehosi kieltä. Mitä kipu tai haaste kehollasi sinulle kertoo?
+Opit löytämään kehosi viisauden ja huomioimaan tärkeät fyysiset elimet kehollasi. Tämä osio opettaa sinulle kiitollisuuden kehoasi kohtaan. Tiesitkö, että kiitollisuus on yksi rakkauden voima ja myös toisinpäin. Mitä enemmän tunnet rakkautta sitä enemmän myös kiitollisuuden tunne nousee mieleen. 
+<br><br>
+
+Lisäksi Muutoksen matkalla opit näkemään eteenpäin tulevaisuuteesi. 
+Miksi ei voi heti samantien nähdä tulevaisuuteen? Koska sinun mielesi ja kehosi ei uskoisi näkemäänsä. Siksi kun ne saattavat kantaa mukana niin paljon rajoittavia uskomuksia ja esteitä etteivät ne edes uskoisi, että tulevaisuuden näkemä voisi pitää paikkansa. 
+<br><br>
+<img src="https://i.imgur.com/kyxTKLP.png" alt="idk">
+Siispä 3 kuukauden aikana opit ohjelmoimaan tulevaisuuttasi koska itsesi rakastamisen avulla luotat jo omiin valintoihin ja eteenpäin menemisen merkkeihin. Luottamuksen ja itsesi rakastamisen kautta näet tulevaisuutesi valoisana, tiedät kuinka mennä eteenpäin itsevarmana ilman stressiä ja epävarmuutta. 
+Tämäkin osa-alue on tärkeä huomioida omalla muutosmatkallasi. 
+<br><br>
+
+KOKONAISUUDESSAAN MUUTOKSEN MATKA valmennus on matka sinuun itseesi! Oletko valmis avaa oven omalle itsellesi, ja aloittaa löytöretken itsesi kanssa? 💗
+<br><br>
+
+Autan yksinkertaisilla menetelmillä sinua sisäistämään omaa itseäsi ja vapautumaan kaikesta siitä tunne kuormasta, joka on itsesi rakastamisen ja eheytymisesi esteenä.
+<br><br>
+
+Valmennus kestää 12 viikkoa ja se käydään läpi itsenäisesti työskentelynä sekä viikoittain Susannan kanssa tapahtuvina tunnin livevalmennuksina Google Meetin videopuheluna. 
+Sinun ei siis tarvitse yksin tätä matkaa kulkea, mutta itsenäisesti sinun tulee panostaa siihen ja tehdä tarvittavat muutokset. Ja ottaa se ensimmäinen askel 💗
+<br><br>
+
+Otatko sen askeleen nyt? 
+<br><br>
+<img src="https://i.imgur.com/H88lofs.png" alt="idk">
+Varaa ensin ilmainen puhelu, jossa käymme läpi seuraavat asiat:
+1. Sinun lähtökohta ( mikä on haasteellisin ja rajoittavia asia elämässäsi)
+2. Mitä valmennus tarkalleen pitää sisällään
+3. Käytännön asiat ja info miten valmennuksessa menetellään
+4. Tärkein! Oletko sinä sopiva tähän valmennukseen ja olenko minä sopiva valmentaa sinua? (Kemiat tulee toimia jotta homma toimii) 
+5. Tämä puhelu ei sido sinua mihinkään. Sinun ei tarvitse puhelun aikana tehdä päätöstä mihinkään suuntaan. Voit kaikessa rauhassa miettiä olisiko tämä matka sinun matka ja sitten vasta tehdä päätös. Jos se ei puhelun aikana tai sen jälkeen sinua millään tavalla resonoi niin sittenhän asia on sillä selvä.
+
+<br><br>
+
+J o t e n
+
+<a class="underline text-red-600" href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIA7AIYAcADBAgCysRcCsENgE4AZv2EQGANgBGARn4gANCCY0acTHAxMANgGVo8JGFoESAXQC%2BNoA
+" target="_blank">VARAA SIIS ENSIN ILMAINEN PUHELU</a>
+jos Muutoksen matka yhtään tässä vaiheessa resonoi sinua. Ja sitten puhelussa tuumaillaan asiaa yhdessä lisää! 
+<br><br>
+<img src="https://i.imgur.com/CrKE2gV.png" alt="idk">
+
+PALAUTTEITA MUUTOKSEN MATKALAISILTA:
+<br><br>
+
+🤍 Arvostus itseäni kohtaan on noussut ja sitä myötä arvostan myös muita ihmisiä aidosti. Olen myös huomannut, että tulevaisuuden suunnittelu ja ihan vain haaveilu on helpompaa, siihen ei liity sellaista negatiivista 'ei se kuitenkaan tapahdu' - ajattelumallia niinkuin aiemmin.
+<br><br>
+
+🤍 On turvallinen ja hyvä olo tehtäviä tehdessä. Susannan tuki on valoisaa ja positiivista. Koen, että minut hyväksytään sellaisena kuin olen ja uskallan sanoa asioita ääneen koska tuntuu ettei tuomitse mistään sanomisista. Iso kiitos Susanna!
+<br><br>
+
+🤍 Valmennus ei ole pitkästyttävää puurtamista. Jokainen tehtäväkirja avaa aina lisää omaa itseä ja pitää mielenkiinnon yllä. 
+<br><br>
+
+🤍 Tehtävissä on myös haastetta ja tekemistä ja niihin pitää tosissaan keskittyä. Ei riitä vaan että tekstit lukee läpi. Se ei vielä tee muutosta. Muutos tapahtuu vasta kun tekee annetut tehtävät. 
+<br><br>
+
+🤍 Olin aluksi hieman skeptinen mutta matkan varrella opein tosi monta asiaa jotka oli rajoittanut itseäni niin monen vuoden ajan että kyllä skeptisyys kaikkosi jo siinä vaiheessa kun ekan kerran tunsin valtavan ihanan rakkaudellisen tunteen itseäni kohtaan. Se oli ihanin tunne ikinä ja miten paljon se antoi itselleni itseni hyväksymistä. Tuli ajatus, että tämä kaikki on mahdollista myös minulle. Tuu ihmeessä sinäkin tälle matkalle mukaan! Tämä muuttaa sinut. Niin se teki minullekin! 
+<br><br>
+
+🤍 Tämä matka oli tuli just niin oikeaan aikaan mulle kun vaihdevuodet ovat alkaneet. Nyt ymmärrän tässä elämäni vaiheessa itseäni ja oman kehoani viestintää paljon paremmin. Ja miten paljon on  merkitystä sillä että tuntee rakkautta itseään kohtaan. Ja miten paljon enemmän kaikkea parasta elämään mahtuu mukaan. I love me! 
+<br><br>
+
+🤍 Jos en silloin olisi tätä muutosmatkaa itselleni antanut, niin olisin varmaan vieläkin samassa tilanteessa väsynyt kaikkeen ja ihmettelemässä, että miten päästä eteenpäin elämässä, voida hyvin ja saada edes jostain pientä valon pilkahdusta elämään. Siis tämä matka antoi niin paljon ja yli odotusteni, että kyllä kannatti kulkea tämä matka kun nyt näkee kaikki asiat paljon antoisammin ja on ihana tunne sisimmässä. 
+<br><br>
+
+Kysy itseltäsi jo nyt tämä kysymys.
+<br><br>
+
+Miltä kuulostaisi, jos voisit kolmen kuukauden kuluttua kokea ja tuntea uskomattomia muutoksia itsessäsi? 
+<br><br>
+
+
+👉Muutoksen matka valmennuksen mentorina ja valmentaja on Susanna Höijer. "Olen myös itse käynyt läpi tämän saman muutosmatkan, joten tiedän mikä toimii ja mikä ei. Olen kehitellyt tähän valmennukseen Sinulle vain kaikista parhaimmat ja tarpeelliset työvälineet, jotta pääset itsesi kanssa eheytymään."
+<br><br>
+
+
+SINUN EI SIIS tarvitse rämpiä enää useita vuosia saavuttaaksesi terveyttä, hyvinvointia ja tasapainoa elämääsi ja kokeilla erilaisia juttuja mistä löytää ne avaimet omaan hyvinvointiin. Vaan voit saavuttaa sen kolmessa kuukaudessa. Vain kolme kuukautta 💗 
+<br><br>
+
+Nähdäänkö Muutoksen matkalla? 
+<br><br>
+
+Varaa tästä <a class="underline text-red-600" href="https://booksalon.fi/hyvinvointikeskus-luxus?booking-state=N4IgzgpgTgbglgYwgYQPYDsAuEAemwgBcA2qJLIhAJIAmRIA7AIYAcADBAgCysRcCsENgE4AZv2EQGANgBGARn4gANCCY0acTHAxMANgGVo8JGFoESAXQC%2BNoA" target="_blank">ILMAINEN PUHELU!</a> Ja aloita oma paras matkasi yhdestä puhelusta! 
+<br><br>
+<img src="https://i.imgur.com/CkzmM5G.png" alt="idk">
+</div>
         </div>
     </main>
-    
-    <style>
-        .bigger{
-            text-transform: uppercase;
-        }
-        p{
-            line-height: 1.6rem !important;
-        }
+<style>
 </style>
