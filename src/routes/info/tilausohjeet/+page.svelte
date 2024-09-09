1703c3fed813b921cc553c84a14a262a1c9d58ac
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-09-09 13:16:14 +0300

    cba2

---
+++ src/routes/info/tilausohjeet/+page.svelte
@@ -0,0 +1,298 @@
+<main class="ml-2 py-10 flex flex-col justify-center align-middle">
+	<div class="grid place-items-center pb-5">
+		<div class="max-w-[100ch] space mx-1 text-lg flex-col gap-3">
+			<h1 class="text-center text-3xl font-bold">KAUPAN TILAUSOHJEET</h1>
+
+			<h3>SYÖTÄ TILAUSTIEDOT:</h3>
+			<p>
+				Täytä nimi - ja yhteystiedot. Jos tilauksesi koskee fyysistä tuotetta, ilmoita tilauksessa
+				ekstra tiedoissa puhelinnumero, jolloin postitoimipaikasta noudettavissa olevasta
+				lähetyksestä voidaan sinulle tekstiviestillä ilmoittaa.
+			</p>
+
+			<h3>VAIHTOEHDOT TOIMITUSTAVOILLE:</h3>
+			<p>Löytyy kunkin tuotteen/palvelun tiedoista.</p>
+
+			<h3>Huom! TUOTE JOKA ON SÄHKÖISESSÄ MUODOSSA:</h3>
+			<ul>
+				<li>
+					<strong>PDF-TIEDOSTO, ÄÄNITE tai SÄHKÖINEN AJANVARAUS, DIGITAALINEN TUOTE:</strong> löydät
+					niiden linkit maksusuorituksen jälkeen ruudulla näkyvällä sivulla. Saat sähköpostiisi tilausvahvistuksen,
+					jossa myös on mukana linkit sähköisiin palveluihin, ladattaviin tiedostoihin tai sähköiseen
+					ajanvaraukseen ohjaava linkki.
+				</li>
+				<li>
+					<strong>HOITOJEN TILAUS:</strong> Sähköpostiin tulevassa tilausvahvistuksessa on linkki nettiajanvaraukseen,
+					josta voit tehdä ajanvarauksen.
+				</li>
+				<li>
+					<strong>ETÄPALVELUJEN TILAUS:</strong> Etäpalvelut toimitetaan sähköpostitse 1-3 arkipäivän
+					kuluessa erikseen ilmoitetulla tavalla. Palvelun tulosta ilmoitetaan erillisellä sähköpostilla.
+					Etäpalveluihin ei ole erillistä ajanvarausta.
+				</li>
+				<li>
+					<strong>KURSSIT JA VALMENNUKSET:</strong> Toimitetaan erikseen ilmoitetulla tavalla joko osittain
+					tai kokonaan sähköpostitse PDF-tiedostona sekä videopuhelujen kautta.
+				</li>
+				<li>
+					<strong>FYYSISET TUOTTEET:</strong> Toimitetaan postin välityksellä 1-3 vrk sisällä tilauksesta.
+					Saat erillisen tiedon toimituksesta sähköpostiisi.
+				</li>
+			</ul>
+
+			<h3>OSTOSKORI:</h3>
+			<p>Ostoskoriin lisätään tuotteita klikkaamalla 'Lisää ostoskoriin.'</p>
+
+			<h3>ALENNUSKOODI:</h3>
+			<p>
+				Ostoskorissa on alennuskoodille oma kuponkisarake, jonne voit syöttää voimassa olevan
+				alennuskoodin.
+			</p>
+
+			<h3>OSTOSKORIN SISÄLTÖ:</h3>
+			<ul>
+				<li>Ostoskorin sisältöä pääset tarkastelemaan ja muuttamaan.</li>
+				<li>Ostoskorista voi poistaa tuotteita 'poista'-painikkeella.</li>
+				<li>
+					Ostoskori näyttää tuotteen summan/tuotteiden yhteissumman ja tilauksen loppusumman myös
+					alennuskoodin jälkeen.
+				</li>
+				<li>Ostoskori löytyy kaupan oikeasta yläkulmasta.</li>
+			</ul>
+
+			<h3>ETSI TUOTE:</h3>
+			<p>
+				Etsiessäsi oikeaa tuotetta kaupasta, kaupan sivulla ylhäällä on hakuohjelma, jonne voit
+				syöttää hoidon tai palvelun nimen ja klikata 'Etsi', niin palvelin etsii sinulle oikean
+				tuotteen.
+			</p>
+
+			<h3>ALEKOODI / LAHJAKORTTI KOODI:</h3>
+			<p>
+				Mikäli sinulla on käytössäsi alekoodi tai lahjakorttikoodi, syötä ostoskorissa koodi sille
+				vaadittuun kuponkikenttään ja aktivoi koodi.
+			</p>
+
+			<h3>TILAUKSEN LÄHETTÄMINEN:</h3>
+			<p>
+				Valittuasi haluamasi tuotteet ostoskoriin, klikkaa "siirry maksamaan". Ilmoita tilaustiedot:
+				nimi, osoite, postinumero, postitoimipaikka, maa, sähköposti, puhelin. Lähetä tilaus.
+				Tilatessasi verkkokaupasta sinun edellytetään tutustuneen ja sitoutuneen kulloinkin voimassa
+				oleviin toimitusehtoihin.
+			</p>
+
+			<h3>TOIMITUSTAPA:</h3>
+			<p>
+				Toimitustapa on ilmoitettu kunkin tuotteen kohdalla erikseen. Toimitustavat aineettomilla
+				tuotteilla: sähköposti lähetys, PDF-tiedostona, linkki äänitteeseen, sähköinen ajanvaraus,
+				videopuheluna, puhelimitse. Fyysisen tuotteen kohdalla lähetys tapahtuu postin mukana tai
+				kirjelähetyksenä.
+			</p>
+
+			<h3>MAKSUTAPA:</h3>
+			<p>Maksuvälineenä käy pankkikortti, Mobilepay, Klarna (Klarnalla on minimiostosraja 25€).</p>
+
+			<h3>TILAUKSEN VAHVISTUS:</h3>
+			<p>
+				Kun olet lähettänyt tilauksesi, saat sähköpostiisi tilausvahvistuksen, josta löydät
+				tilauksen tiedot ja linkit kyseiseen tilattuun tuotteeseen. Sähköiset ja aineettomat
+				tuotteet löydät myös samantien sivulta, joka aukeaa ruudulle maksusuorituksen jälkeen.
+			</p>
+
+			<h3>MAKSUJÄRJESTELMÄ:</h3>
+			<p>Kaupan maksujärjestelmänä toimii Stripe.</p>
+
+			<h3>TILAUKSEN TOIMITUS:</h3>
+			<p>
+				Etäpalvelujen ja tuotteiden tilaus toimitetaan 1-3 vuorokauden kuluttua ostohetkestä. Mikäli
+				kyseessä on tilaustyönä valmistettu tuote, tuotteen toimitusaika ilmoitetaan tuotteen
+				tilaustiedoissa. Tilauksesi toimituksesta voit kysyä lisätietoja sähköpostilla: <a
+					href="mailto:info.hyvinvointikeskusluxus@gmail.com"
+					>info.hyvinvointikeskusluxus@gmail.com</a
+				>
+			</p>
+
+			<hr />
+
+			<h1>KAUPAN TIETOSUOJASELOSTE</h1>
+
+			<p><strong>Voimassa 07.09.2024 alkaen</strong><br />Hyvinvointikeskus Luxus Verkkokauppa</p>
+
+			<p>
+				Hyvinvointikeskus Luxus, 1773403-5 myy tuotteita, hoitoja, hyvinvointipalveluita,
+				digitaalisia tuotteita, kursseja ja valmennuksia. Pidätämme oikeuden hintojen ja
+				toimituskulujen muutoksiin.
+			</p>
+
+			<h2>Tilaaminen:</h2>
+			<p>
+				Tuotteet ja palvelut tilataan verkkokaupasta siirtämällä ne ostoskoriin ja maksamalla
+				ostoskorin sisältö kassapalvelussa. Kaikki asiakastiedot käsitellään luottamuksellisesti.
+				Tilauksen yhteydessä kysyttyjä yhteystietoja ei käytetä muuhun kuin tilauksen toimitukseen
+				tai siinä ilmenevien epäselvyyksien selvittämiseen, ellei erikseen toisin mainita.
+				Tilatessasi verkkokaupasta sinun edellytetään tutustuneen ja sitoutuneen kulloinkin voimassa
+				oleviin toimitusehtoihin.
+			</p>
+
+			<h2>Hinnat:</h2>
+			<p>
+				Ostosopimus tehdään tilaushetkellä voimassa olevien hintojen mukaan. Poikkeukselliset hinnat
+				(erikoistarjoukset, alennusmyynnit, ym. hinnat) ilmenevät verkkokaupasta tai yksittäisen
+				hyödykkeen esittelysivulta. Verkkokauppa voi halutessaan muuttaa hintoja milloin tahansa
+				ilman erillistä ilmoitusta, mutta hinnankorotukset koskevat vain tilauksia, jotka on tehty
+				tällaisten muutosten jälkeen. Hintatietovirheiden kanssa menetellään annetun lain 32 §:n
+				pykälän mukaan.
+			</p>
+
+			<h3>Tuotteiden saatavuus:</h3>
+			<p>
+				Verkkokauppa ei anna takuuta verkkokaupassa olevien tuotteiden saatavuudesta. Verkkokaupassa
+				on nähtävillä ja tilattavissa vain kulloinkin saatavilla olevat tuotteet tai tuotteiden
+				kohdalla lukee, jos tuote on tilapäisesti loppu.
+			</p>
+
+			<h3>Tuotteiden takuu:</h3>
+			<p>
+				Fyysisissä tuotteissa ei ole takuuta ellei siitä ole erikseen mainintaa. Tiedot takuusta
+				ilmenee erikseen tuotteiden tietojen yhteydessä. Takuu ilmoitetaan myös mikäli jokin taho
+				aikaisemmassa myyntiportaassa (maahantuoja, valmistaja) on päättänyt antaa tuotteelle
+				takuun. Takuu ei rajoita kuluttajansuojalaissa säädettyä virhevastuuta.
+			</p>
+
+			<h3>Vastuu vastaanotetusta tavarasta:</h3>
+			<p>
+				Kun kuluttaja on vastaanottanut tavaran, hänen on pidettävä se olennaisesti muuttumattomana
+				ja vähentymättömänä siihen saakka, kunnes hän on päättänyt pitää tavaran. Jos palautettua
+				tuotetta on vastoin ostajan laissa säädettyä huolenpitovelvollisuutta käytetty tai muokattu,
+				pidätämme oikeuden vähentää palautettavasta summasta tuotteen kärsimää arvonalennusta
+				vastaava summa, aina tuotteen täyteen hintaan saakka.
+			</p>
+
+			<h3>Maksaminen ja maksutavat:</h3>
+			<p>Pankkikortit, Mobilepay, Apple Pay, Klarna.</p>
+
+			<h3>Tilaus- ja maksuvahvistus:</h3>
+			<p>
+				Onnistuneen tilauksen jälkeen sähköpostiisi lähetetään tilausvahvistus. Vahvistusviestien
+				lähettäminen edellyttää sähköpostiosoitteen antamista tilauksen yhteydessä.
+			</p>
+
+			<h3>Toimitustavat ja -kulut:</h3>
+			<p>
+				Verkkokaupassa voi tehdä vain kertaostoksia. Tilauksesi fyysisten tuotteiden toimituskulut
+				näet toimitus- ja maksutavan valintasivulla. Toimitamme fyysiset tuotetilaukset:
+			</p>
+			<ul>
+				<li>Posti</li>
+				<li>Kirjelähetys</li>
+			</ul>
+			<p>Lähetyksiä voi seurata lähetystunnuksella.</p>
+			<p>
+				Toimitamme aineettomat tilaukset kuten hoidot, palvelut, valmennukset, ym. sähköiset
+				tilaukset:
+			</p>
+			<ul>
+				<li>
+					Ajanvarauksella (asiakkaan tehtäväksi jää varata palvelun/hoidon aika sovitun ajan sisällä
+					mikäli palvelulle on ilmoitettu erikseen lunastus aika)
+				</li>
+				<li>Sähköpostitse</li>
+				<li>Puhelimitse (asiakkaan ajanvarauksella)</li>
+				<li>Videopuheluna</li>
+				<li>Linkkinä</li>
+				<li>PDF tiedostona</li>
+				<li>Muun erikseen ilmoitetulla tavalla</li>
+			</ul>
+
+			<h3>Toimitusaika:</h3>
+			<p>
+				Yleisimmät toimitusaikamme tuotteilla ja palveluilla ovat tilauksesta ja toimitustavasta
+				riippuen 1-3 päivää. Mikäli kysessä on tilaustyönä toteutettava tuote, tarkemmat ajat
+				ilmoitetaan tuotevaihtoehtojen yhteydessä. Emme vastaa ylivoimaisen esteen aiheuttamista
+				viivästymisistä tai viivästysten aiheuttamista välillisistä haitoista. Toimitusaika
+				tilaustyönä tehdyille tuotteille on 1-2 viikkoa.
+			</p>
+
+			<h3>Peruutusoikeus ja palautusehdot:</h3>
+			<p>
+				Kuluttajansuojalaki mukainen 14 päivän peruutusoikeus <strong>ei koske</strong> aineettomia palveluita,
+				koska asiakas saa välittömästi käyttöönsä palvelut, jolloin Hyvinvointikeskus Luxus on täyttänyt
+				sopimusvelvoitteensa kokonaan. Peruutusoikeus ei koske seuraavia:
+			</p>
+			<ul>
+				<li>Hyvinvointipalveluja</li>
+				<li>Äänitteitä</li>
+				<li>Valmennuksia</li>
+				<li>Kursseja</li>
+				<li>Hoitoja</li>
+				<li>Lahjakortteja</li>
+				<li>Digitaalista sisältöä ja palveluita</li>
+				<li>Sähköpostitse lähetettyjä etäpalveluita</li>
+				<li>PDF-tiedostoja</li>
+			</ul>
+			<p>
+				Peruutusoikeus ei myöskään koske asiakkaan toivomuksesta valmistettua tai muunneltua
+				tuotetta tai syötäviä tuotteita.
+			</p>
+
+			<p>
+				Sinulla on kuluttajansuojalain mukainen oikeus peruuttaa fyysisen tuotetilauksesi 14
+				vuorokauden kuluessa tilauksen vastaanottamisesta. Palautuksessa ole ensin yhteydessä
+				yrittäjään.
+			</p>
+			<p>
+				Peruutusilmoitukseen sinun tulee merkitä nimesi, osoitteesi, tilauspäivä, tuote ja se tulee
+				lähettää sähköpostilla osoitteeseen info.hyvinvointikeskusluxus@gmail.com tai postitse alla
+				olevaan palautusosoitteeseen. Tuote tulee palauttaa postitse 14 päivän kuluessa
+				peruutusilmoituksesta käyttämättömänä alkuperäiskunnossa ja alkuperäispakkauksessa.
+				Palautuskustannukset ovat asiakkaan vastuulla. Liitä kopio peruutusilmoituksesta
+				tuotepalautuksen yhteyteen. Jos palautettua tuotetta on vastoin ostajan laissa säädettyä
+				huolenpitovelvollisuutta käytetty tai muokattu, pidätämme oikeuden vähentää palautettavasta
+				summasta tuotteen kärsimää arvonalennusta vastaava summa, aina tuotteen täyteen hintaan
+				saakka. Palauta kaikki palautettavat tuotteet samassa lähetyksessä. Vasta, kun palautettava
+				tuote/tuotteet ovat meillä perillä, palautamme rahat tilillesi.
+			</p>
+
+			<h3>Palautusosoite:</h3>
+			<p>
+				Hyvinvointikeskus Luxus<br />
+				Kaltsilantie 118<br />
+				38220 SASTAMALA<br />
+				Puh. 040 7249 887<br />
+				info.hyvinvointikeskusluxus@gmail.com
+			</p>
+
+			<h3>Ongelmatilanteet:</h3>
+			<p>
+                Mikäli fyysisen tuote on kadonnut tai vioittunut kuljetuksen aikana tai se ei muuten vastaa tilaustasi, tulee sinun ilmoittaa virheestä viimeistään 14 päivän kuluessa kirjallisesti Peruutusoikeus ja palautusehdot -kohdassa mainittuun osoitteeseen tai soittamalla yhteystiedoissa mainittuun puhelinnumeroon. Jos paketti on vioittunut Postissa, on tuotteesta tehtävä välittömästi reklamaatio suoraan Postiin.
+			</p>
+
+            <h3>Tekijänoikeudet:</h3>
+            <p>
+				Asiakas ei saa jakaa, myydä, alilisensoida tai muutoin siirtää Hyvinvointikeskus Luxusen aineettomia palveluita kolmannelle osapuolelle tai hyödyntää materiaaleja kaupallisesti. 
+
+            </p>
+            <h3>Ohjelmien ja palveluiden päivitykset:</h3>
+            <p>
+                Hyvinvointikeskus Luxus voi päivittää tai muuttaa tarpeen mukaan palveluita, kursseja, valmennuksia ja hoitoja vastatakseen ajankohtaisiin tarpeisiin.
+
+            </p>
+            <h3>Vastuuvapaus:</h3>
+            <p>
+                Hyvinvointipalveluita, hoitoja, kursseja, valmennuksia ja muita aineettomia palveluita tilattaessa Hyvinvointikeskus Luxus ei ole vastuussa Asiakkaan mahdollisista henkisistä tai fyysistä haasteista eikä Hyvinvointikeskus Luxus
+anna takeita tai ole vastuussa siitä, että palvelut, hoidot, kurssit ja valmennukset vastaanottamisen ja niiden suorittamisen jälkeen saavuttaa mahdollisesti tavoittelemiaan muutoksia. Asiakas on vastuussa saamiensa tietojen soveltamisesta ja niiden perusteella tehtyjen päätösten seurauksista.
+            </p>
+            <h3>Sovellettava laki ja riitojen ratkaiseminen:</h3>
+            <p>
+                Verkkokauppa soveltaa toimintaansa Suomen lakia. Kuluttaja voi saattaa kauppaa koskevan riita-asian, jota ei saada neuvotteluteitse ratkaistua, kuluttajariitalautakunnan (www.kuluttajariita.fi) käsiteltäväksi.
+            </p>
+		</div>
+	</div>
+</main>
+<style>
+    h3{
+        font-weight: bold;
+		margin-bottom: 10px;
+		margin-top: 10px;
+    }
+</style>
