430460e01cae2aa3c20fb1033da0d410bc6dbf25
Niko Junttila <nikosamujunttila@gmail.com>
2025-08-28 13:14:23 +0300

    images new

--- src/routes/matka/+page.svelte
+++ src/routes/matka/+page.svelte
@@ -1,192 +1,34 @@
 <script>
 import { timereserveURL } from "$lib/constants";
+
+const imgArr = [
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F1.png?alt=media&token=d730110d-da54-4e48-8d0e-6981ed77bbca",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F2.png?alt=media&token=92c60aa5-f63a-4004-ba95-5f09054b223c",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F3.png?alt=media&token=f08b5e4f-e0e7-4ee3-8b2e-5838dee2f115",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F4.png?alt=media&token=cd2639fe-6653-48d1-98e0-7d8ae4403dc6",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F5.png?alt=media&token=f92bf045-4e31-4743-92d6-0f0b1a8c7150",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F6.png?alt=media&token=89502323-06b9-491d-b6af-42735c4058dc",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F7.png?alt=media&token=c3880a6b-c02d-4536-9085-ce83a3b29773",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F8.png?alt=media&token=b7e08bae-b9c5-4dad-81fc-f166fce125db",
+  "https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2F9.png?alt=media&token=816e5a0d-8f5f-45ee-9753-c96b5fcda567"
+]
 </script>
-<main class="ml-2 py-10 flex flex-col justify-center align-middle">
+<main class="ml-2 py-2 flex flex-col justify-center align-middle">
 	<div class="grid place-items-center pb-5">
 		<div class="max-w-[100ch] space mx-1 text-lg">
-			<h1 class="text-3xl font-bold">TUTUSTUMINEN OMAAN SISIMPÄÄSI?</h1>
-			<img src="https://i.imgur.com/41nqpOT.png" alt="idk" />
-			<br /><br />
-			Tämä uudistettu Muutoksen matka valmennus valmistaa sinulle tukevan perustan, joka vahvistaa sinut
-			henkisesti ja fyysisesti. Sinulla on tämän valmennuksen käytyäsi aina oma työkalupakki mukana josta
-			voit kulloiseenkin tarpeeseen tai matkan varrella olevaan haasteeseen ottaa oman työvälineen jolla
-			sujuvasti pääset eteenpäin.
-			<br /><br />
+      {#each imgArr as img}
+        <img loading="lazy" src={img} alt="susanna">
+      {/each}
 
-			Muutoksen matkan tarkoitus on valmentaa sinut aidosti rakastamaan itseäsi. Ei riitä että
-			ajattelee rakastavansa itseään jos teot eivät ole samassa linjassa ajatuksen kanssa.
-			<br /><br />
-
-			Yksi parhaimmista keinoista selvittää se että rakastaako itseään on mennä peilin eteen ja
-			katsoa miltä oma peilikuva tuntuu. Tuntuuko se, että "jes, olen upea" vai tuntuuko se, että "
-			voi ei, en edes välitä katsoa itseäni peilistä, tai onko ajatuksesi enemmän sitä luokkaa ,
-			että "se on turhaa katsoa peilistä itseä". Tai kenties "ei sieltä kuitenkaan mitää hyvää
-			näy,".
-			<br /><br />
-
-			Myös se kertoo paljon, että millä tavalla itsestään ajattelee. Onko tapana enemmän moittia tai
-			sättiä itseä kuin olla kiitollinen itsestään ja rakastaa itseään. Kun aidosti rakastaa itseä
-			osaa tehdä itseä kunnioittavia ja arvostavia valintoja. Itseään kunnioittava ei koskaan moiti
-			eikä mitätöi itseä.
-			<br /><br />
-			<img src="https://i.imgur.com/flGpNjz.png" alt="idk" />
-			👉 Jos mielit auttaa aina kaikessa muita ja etenkin laitat kaikki muut aina edelle oman elämäsi
-			ja itsesi kustannuksella, ei tarkoita sitä, että se olisi rakkautta sinua itseäsi kohtaan. Päinvastoin
-			se on tuho energiaa sinua itseäsi kohtaan.
-			<br /><br />
-			<p class="font-bold text-2xl">MUUTOKSEN MATKAN</p>
-			<br /><br />
-			<img src="https://i.imgur.com/pztzqCg.png" alt="idk" />
-			1. kuukauden osa-alueella opit löytämään rajoittavat tunteet ja ohjelmoinnit itseltäsi, opit vapautuu
-			suurimmista peloista ja merkityksettömyydestä ja niistä kaikista jotka ovat estäneet sinua olemasta
-			omassa voimassasi, riittävä, ja tärkeä. Tämän ensimmäinen kuukauden toisella viikolla opit rakastamaan
-			itseäsi ja tiedät, miltä se tunne kuuluu tuntua.
-			<br /><br />
-
-			Silloin jää pois toisten miellyttämiset, itsesi pienentäminen ja näkymättömän lapsen roolin
-			kantaminen. Nouset omaan voimaasi, kukoistukseen ja ihanaan itseäsi kohtaan tuntemaan
-			rakkauden voimaan.
-
-			<br /><br />
-			<img src="https://i.imgur.com/TqkcnGO.png" alt="idk" />
-			MUUTOKSEN MATKA valmentaa sinua rakastaa myös oma kehosi terveeksi. Toisen kuukauden osa-alue koostuu
-			fyysisen kehon aiheista ja siitä mitä kaikkia tunteita fyysinen keho pitää sisällään esimerkiksi
-			kipujen ja oireilun kautta. Opit fyysisen kehon ja tunne ohjelmoinnin yhteistyöstä tärkeät seikat.
-			Tämän osion käytyäsi tunnet kehosi terveyden ja tunnet jatkossa sinun sisäisen ja fyysisen kehosi
-			yhteenkuuluvuuden. Opit lukemaan kehosi kieltä. Mitä kipu tai haaste kehollasi sinulle kertoo?
-			Opit löytämään kehosi viisauden ja huomioimaan tärkeät fyysiset elimet kehollasi. Tämä osio opettaa
-			sinulle kiitollisuuden kehoasi kohtaan. Tiesitkö, että kiitollisuus on yksi rakkauden voima ja
-			myös toisinpäin. Mitä enemmän tunnet rakkautta sitä enemmän myös kiitollisuuden tunne nousee mieleen.
-			<br /><br />
-
-			Lisäksi Muutoksen matkalla opit näkemään eteenpäin tulevaisuuteesi. Miksi ei voi heti
-			samantien nähdä tulevaisuuteen? Koska sinun mielesi ja kehosi ei uskoisi näkemäänsä. Siksi kun
-			ne saattavat kantaa mukana niin paljon rajoittavia uskomuksia ja esteitä etteivät ne edes
-			uskoisi, että tulevaisuuden näkemä voisi pitää paikkansa.
-			<br /><br />
-			<img src="https://i.imgur.com/kyxTKLP.png" alt="idk" />
-			Siispä 3 kuukauden aikana opit ohjelmoimaan tulevaisuuttasi koska itsesi rakastamisen avulla luotat
-			jo omiin valintoihin ja eteenpäin menemisen merkkeihin. Luottamuksen ja itsesi rakastamisen kautta
-			näet tulevaisuutesi valoisana, tiedät kuinka mennä eteenpäin itsevarmana ilman stressiä ja epävarmuutta.
-			Tämäkin osa-alue on tärkeä huomioida omalla muutosmatkallasi.
-			<br /><br />
-
-			KOKONAISUUDESSAAN MUUTOKSEN MATKA valmennus on matka sinuun itseesi! Oletko valmis avaa oven
-			omalle itsellesi, ja aloittaa löytöretken itsesi kanssa? 💗
-			<br /><br />
-
-			Autan yksinkertaisilla menetelmillä sinua sisäistämään omaa itseäsi ja vapautumaan kaikesta
-			siitä tunne kuormasta, joka on itsesi rakastamisen ja eheytymisesi esteenä.
-			<br /><br />
-
-			Valmennus kestää 12 viikkoa ja se käydään läpi itsenäisesti työskentelynä sekä viikoittain
-			Susannan kanssa tapahtuvina tunnin livevalmennuksina Google Meetin videopuheluna. Sinun ei
-			siis tarvitse yksin tätä matkaa kulkea, mutta itsenäisesti sinun tulee panostaa siihen ja
-			tehdä tarvittavat muutokset. Ja ottaa se ensimmäinen askel 💗
-			<br /><br />
-
-			Otatko sen askeleen nyt?
-			<br /><br />
-			<img src="https://i.imgur.com/H88lofs.png" alt="idk" />
-			Varaa ensin ilmainen puhelu, jossa käymme läpi seuraavat asiat: 1. Sinun lähtökohta ( mikä on haasteellisin
-			ja rajoittavia asia elämässäsi) 2. Mitä valmennus tarkalleen pitää sisällään 3. Käytännön asiat
-			ja info miten valmennuksessa menetellään 4. Tärkein! Oletko sinä sopiva tähän valmennukseen ja
-			olenko minä sopiva valmentaa sinua? (Kemiat tulee toimia jotta homma toimii) 5. Tämä puhelu ei
-			sido sinua mihinkään. Sinun ei tarvitse puhelun aikana tehdä päätöstä mihinkään suuntaan. Voit
-			kaikessa rauhassa miettiä olisiko tämä matka sinun matka ja sitten vasta tehdä päätös. Jos se ei
-			puhelun aikana tai sen jälkeen sinua millään tavalla resonoi niin sittenhän asia on sillä selvä.
-
-			<br /><br />
-
-			J o t e n
-
-    <a class="font-bold mb-4 text-blue-600" target="_blank" href="">ajanvaraus</a>
-			<a
-				class="underline text-red-500"
-				href="{`${timereserveURL}book/4?lang=fi&id=12`}"
-				target="_blank">VARAA SIIS ENSIN ILMAINEN PUHELU</a
-			>
-			jos Muutoksen matka yhtään tässä vaiheessa resonoi sinua. Ja sitten puhelussa tuumaillaan asiaa
-			yhdessä lisää!
-			<br /><br />
-			<img src="https://i.imgur.com/CrKE2gV.png" alt="idk" />
-
-			PALAUTTEITA MUUTOKSEN MATKALAISILTA:
-			<br /><br />
-
-			🤍 Arvostus itseäni kohtaan on noussut ja sitä myötä arvostan myös muita ihmisiä aidosti. Olen
-			myös huomannut, että tulevaisuuden suunnittelu ja ihan vain haaveilu on helpompaa, siihen ei
-			liity sellaista negatiivista 'ei se kuitenkaan tapahdu' - ajattelumallia niinkuin aiemmin.
-			<br /><br />
-
-			🤍 On turvallinen ja hyvä olo tehtäviä tehdessä. Susannan tuki on valoisaa ja positiivista.
-			Koen, että minut hyväksytään sellaisena kuin olen ja uskallan sanoa asioita ääneen koska
-			tuntuu ettei tuomitse mistään sanomisista. Iso kiitos Susanna!
-			<br /><br />
-
-			🤍 Valmennus ei ole pitkästyttävää puurtamista. Jokainen tehtäväkirja avaa aina lisää omaa
-			itseä ja pitää mielenkiinnon yllä.
-			<br /><br />
-
-			🤍 Tehtävissä on myös haastetta ja tekemistä ja niihin pitää tosissaan keskittyä. Ei riitä
-			vaan että tekstit lukee läpi. Se ei vielä tee muutosta. Muutos tapahtuu vasta kun tekee
-			annetut tehtävät.
-			<br /><br />
-
-			🤍 Olin aluksi hieman skeptinen mutta matkan varrella opein tosi monta asiaa jotka oli
-			rajoittanut itseäni niin monen vuoden ajan että kyllä skeptisyys kaikkosi jo siinä vaiheessa
-			kun ekan kerran tunsin valtavan ihanan rakkaudellisen tunteen itseäni kohtaan. Se oli ihanin
-			tunne ikinä ja miten paljon se antoi itselleni itseni hyväksymistä. Tuli ajatus, että tämä
-			kaikki on mahdollista myös minulle. Tuu ihmeessä sinäkin tälle matkalle mukaan! Tämä muuttaa
-			sinut. Niin se teki minullekin!
-			<br /><br />
-
-			🤍 Tämä matka oli tuli just niin oikeaan aikaan mulle kun vaihdevuodet ovat alkaneet. Nyt
-			ymmärrän tässä elämäni vaiheessa itseäni ja oman kehoani viestintää paljon paremmin. Ja miten
-			paljon on merkitystä sillä että tuntee rakkautta itseään kohtaan. Ja miten paljon enemmän
-			kaikkea parasta elämään mahtuu mukaan. I love me!
-			<br /><br />
-
-			🤍 Jos en silloin olisi tätä muutosmatkaa itselleni antanut, niin olisin varmaan vieläkin
-			samassa tilanteessa väsynyt kaikkeen ja ihmettelemässä, että miten päästä eteenpäin elämässä,
-			voida hyvin ja saada edes jostain pientä valon pilkahdusta elämään. Siis tämä matka antoi niin
-			paljon ja yli odotusteni, että kyllä kannatti kulkea tämä matka kun nyt näkee kaikki asiat
-			paljon antoisammin ja on ihana tunne sisimmässä.
-			<br /><br />
-
-			Kysy itseltäsi jo nyt tämä kysymys.
-			<br /><br />
-
-			Miltä kuulostaisi, jos voisit kolmen kuukauden kuluttua kokea ja tuntea uskomattomia muutoksia
-			itsessäsi?
-			<br /><br />
-
-			👉Muutoksen matka valmennuksen mentorina ja valmentaja on Susanna Höijer. "Olen myös itse
-			käynyt läpi tämän saman muutosmatkan, joten tiedän mikä toimii ja mikä ei. Olen kehitellyt
-			tähän valmennukseen Sinulle vain kaikista parhaimmat ja tarpeelliset työvälineet, jotta pääset
-			itsesi kanssa eheytymään."
-			<br /><br />
-
-			SINUN EI SIIS tarvitse rämpiä enää useita vuosia saavuttaaksesi terveyttä, hyvinvointia ja
-			tasapainoa elämääsi ja kokeilla erilaisia juttuja mistä löytää ne avaimet omaan hyvinvointiin.
-			Vaan voit saavuttaa sen kolmessa kuukaudessa. Vain kolme kuukautta 💗
-			<br /><br />
-
-			Nähdäänkö Muutoksen matkalla?
-			<br /><br />
-
-			Varaa tästä
-			<a
-				class="underline text-red-500"
-				href={`${timereserveURL}book/4?lang=fi&id=12`}
-				target="_blank">ILMAINEN PUHELU!</a
-			>
+			<p class="py-2 text-lg font-bold">Varaa tästä
 			Ja aloita oma paras matkasi yhdestä puhelusta!
-			<br /><br />
-            <div class="mb-4 text-center">
-                <a class="text-2xl font-bold text-red-500 underline" href="https://firebasestorage.googleapis.com/v0/b/hyvinvointikeskusluxus1.appspot.com/o/statics%2FMuutoksen%20matka%20valmennuksen%20INFO%20(3).pdf?alt=media&token=4d9e6325-3a09-4961-af5f-797f3eb40e70" target="_blank">KATSO LISÄÄ TÄSTÄ INFOSTA</a>
-            </div>
-			<img src="https://i.imgur.com/CkzmM5G.png" alt="idk" />
+        </p>
+									<iframe 
+							class="w-full h-96 rounded-lg border-2 border-purple-100" 
+							src="{timereserveURL}book/4?lang=fi&id=12" 
+							title="ajanvaraus">
+						</iframe>
+			
 		</div>
 
 	</div>
